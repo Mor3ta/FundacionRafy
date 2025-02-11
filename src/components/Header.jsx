2c9d8a8f1ea2af4dca598c1197892259beb98b1a
Moreta04 <moreta.concepcion@gmail.com>
2025-02-11 00:31:14 -0400

    cambios al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,33 +6,12 @@ const Header = () => {
 
   return (
     <header className="header">
-    {/* Botón de menú para móviles */}
-    <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-        ☰
-      </button>
-
-
       {/* Sección del logo */}
       <div className="logo">
         <img src="./logo.png" alt="Logo de la Fundación Rafy" />
-        <h3>FUNDACION RAFY</h3>
+        <h3>FUNDACION RAFY BUENO </h3>
       </div>
 
-    
-
-      {/* Sección del menú */}
-      <nav className={`menu ${menuOpen ? 'open' : ''}`}>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/contacto">Contacto</a></li>
-          <li><a href="/sobre-nosotros">Sobre Nosotros</a></li>
-        </ul>
-      </nav>
-
-      {/* Botón de inscripción al torneo */}
-      <div className="inscripcion">
-        <Link to="/inscripcion-torneo" className="inscripcion-button">Inscripción Torneo</Link>
-      </div>
     </header>
   );
 };
